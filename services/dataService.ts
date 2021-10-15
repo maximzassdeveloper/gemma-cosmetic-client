f29ab89ac755517c341122d26960cf38853b0638
Maxim Zass <fantom.zms14@gmail.com>
2021-10-15 16:25:46 +0300

    test partners

--- services/dataService.ts
+++ services/dataService.ts
@@ -1,9 +1,8 @@
 import axios from 'axios'
-import { SERVER_URL } from '../utils/config'
 
 export const fetchData = async (url: string) => {
   try {
-    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/api${url}`, { withCredentials: true })
+    const { data } = await axios.get(`https://server.gemmainrussia.ru/api${url}`, { withCredentials: true })
     return data
   } catch {
     return null
