c912c85d06f5590ffbb478b3692b12690e23d122
Maxim Zass <fantom.zms14@gmail.com>
2021-11-02 20:19:12 +0300

    e

--- components/sections/ProductSection.tsx
+++ components/sections/ProductSection.tsx
@@ -12,21 +12,22 @@ interface ProductSectionProps {
 
 export const ProductSection: FC<ProductSectionProps> = ({ products, title }) => {
 
-  if (!products || !products.length) return null
-
+  
   const [count, setCount] = useState(5)
   
   const max1250 = useMediaQuery('(max-width: 1250px)')
   const max950 = useMediaQuery('(max-width: 950px)')
   const max700 = useMediaQuery('(max-width: 700px)')
-
+  
   useEffect(() => {
     if (max700) return setCount(2)
     else if (max950) return setCount(3)
     else if (max1250) return setCount(4)
     else setCount(5)
   }, [max1250, max950, max700])
-
+  
+  if (!products || !products.length) return null
+  
   return (
     <SliderSection className="product-section products" title={title} count={count}>
       {products.map(product => 
